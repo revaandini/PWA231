ccf17ba01d975333cfbbaf98f46e9fe6fc550bc9
revaandini <revaandini931@gmail.com>
2023-12-06 19:43:24 +0700

    ini tugas PHP Reva Andini

---
+++ Chapter08/README.md/TugasPHP/714230005_RevaAndiniBusra_TugasPHP/komentar.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title> Penulisan Komentar ?PHP</title>
+</head>
+<body>
+<h2>Struktur Komentar PHP<title>
+</head>
+<body>
+<h2>Struktur Penulisan PHP</h2>
+<?php
+// Apakah anda melihat aku di browser ?
+?>
+</body>
+</html>
---
+++ Chapter08/README.md/TugasPHP/714230005_RevaAndiniBusra_TugasPHP/struktur_php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Strukrut Penulisan PHP</title>
+</head>
+<body>
+<h2>Struktur Penulisan PHP </h2>
+<?php
+   ccho("Selamat datang di kawasan PHP!")
+?>
+</body>
+</html>
